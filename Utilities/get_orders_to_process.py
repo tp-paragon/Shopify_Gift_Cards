b55b3f4cb5f24320827bedbde16c5986d779be32
tp-paragon <tpettersen@paragonsports.com>
2026-04-30 12:09:55 -0400

    skip order results that have updatedAt before the search window

--- Utilities/get_orders_to_process.py
+++ Utilities/get_orders_to_process.py
@@ -1,4 +1,5 @@
 from typing import List
+from datetime import datetime
 
 from Shopify_API.execute_api_call import execute_api_call
 from GraphQL_Operations.get_gql_op import get_gql_op
@@ -8,6 +9,8 @@ def get_orders_to_process(updated_since_timestamp: str) -> List[tuple]:
     orders_query_cursor = None
     query_has_next_page = True
     orders = []
+    dt_format = "%Y-%m-%dT%H:%M:%SZ"
+    dt_start = datetime.strptime(updated_since_timestamp, dt_format)
     query = (f"updated_at:>={updated_since_timestamp} "
              "AND fulfillment_status:fulfilled "
              "AND delivery_method:pick-up")
@@ -19,6 +22,11 @@ def get_orders_to_process(updated_since_timestamp: str) -> List[tuple]:
         orders_res = execute_api_call(orders_query, orders_query_variables)
         orders_batch = orders_res["orders"]["nodes"]
         for order in orders_batch:
+            updated_at = order["updatedAt"]
+            dt_order = datetime.strptime(updated_at, dt_format)
+            if dt_order < dt_start:
+                print(f"Order {order["name"]} is too old - {updated_at}", flush=True)
+                continue
             order_tuple = (order["id"], order["name"], order["createdAt"])
             orders.append(order_tuple)
         query_has_next_page = orders_res["orders"]["pageInfo"]["hasNextPage"]
